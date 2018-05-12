4f26adf30aff56570d7e0e8c17451a283d268340
Anas Waseem <anas.waseem@sigos.com>
2018-05-13 01:11:22 +0500

    Project Converted to Maven

---
+++ src/main/java/booth_ai/CloudinaryAPI.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booth_ai;
+import com.cloudinary.*;
+import com.cloudinary.utils.ObjectUtils;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Map;
+/**
+ *
+ * @author AWA
+ */
+public class CloudinaryAPI {
+    
+} 

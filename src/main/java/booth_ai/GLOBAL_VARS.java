0a13e92c6a193595098466155601b9e57d92a82c
Anas Wasim <anas@attribes.com>
2018-06-22 17:33:41 +0500

    Global Vars Template Upload

---
+++ src/main/java/booth_ai/GLOBAL_VARS.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2018 xterminate
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package booth_ai;
+
+/**
+ *
+ * @author xterminate
+ */
+public class GLOBAL_VARS {
+    public static String LBP_FACECASCADE = "";
+    public static String HAAR_SMILECASCADE = "";
+    public static String OUTPUT_FILE = "";
+    public static String USBCPY_SCRIPT_PATH = "";
+    public static String GMAIL_USERNAME = "";
+    public static String GMAIL_PASSWORD = "";
+    
+}
